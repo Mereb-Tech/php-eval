a40290983304568021ff47e2283d38d12f63b2a5
Kirubel25 <kirubel667@gmail.com>
2024-04-25 11:44:37 +0300

    controller logic added

---
+++ src/controller/getSecureRandom.php
@@ -0,0 +1,24 @@
+<?php
+
+// Include utils.php to use the getSecureRandom function
+use General\Utils;
+
+// Set headers to indicate JSON response
+header('Content-Type: application/json');
+
+// Get parameters from request (if any)
+$min = isset($_GET['min']) ? (int)$_GET['min'] : 0;
+$max = isset($_GET['max']) ? (int)$_GET['max'] : PHP_INT_MAX;
+
+// Validate parameters
+if ($min < 0 || $max < 0 || $min > $max) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Invalid parameters']);
+    exit;
+}
+
+// Generate random number
+$random_number = Utils::getSecureRandom($min, $max);
+
+// Return JSON response
+return json_encode(['random_number' => $random_number]);
