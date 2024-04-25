79853027dde4a59142cd651727012a98aa6d7528
Kirubel25 <kirubel667@gmail.com>
2024-04-25 16:34:36 +0300

    request validation done and comment

--- api/getSecureRandom.php
+++ api/getSecureRandom.php
@@ -3,6 +3,7 @@
 require_once '../src/middlewares/header.php';
 require_once '../src/controller/SecureRandomController.php';
 
-
 $secureRandomController = new SecureRandomController();
+
+// Call the getSecureRandom method to generate secure random number
 $secureRandomController->getSecureRandom();
