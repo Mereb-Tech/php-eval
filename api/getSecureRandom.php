4c173e8c00fb1b5a982f84faa5214b240e4100d6
Kirubel25 <kirubel667@gmail.com>
2024-04-25 16:21:33 +0300

    task one and two final

--- api/getSecureRandom.php
+++ api/getSecureRandom.php
@@ -3,5 +3,6 @@
 require_once '../src/middlewares/header.php';
 require_once '../src/controller/SecureRandomController.php';
 
+
 $secureRandomController = new SecureRandomController();
 $secureRandomController->getSecureRandom();
--- src/controller/SecureRandomController.php
+++ src/controller/SecureRandomController.php
@@ -1,25 +1,32 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 use General\Utils;
 
 class SecureRandomController
 {
     public function getSecureRandom()
     {
-        // Get parameters from request (if any)
-        $min = isset($_GET['min']) ? (int)$_GET['min'] : 0;
-        $max = isset($_GET['max']) ? (int)$_GET['max'] : PHP_INT_MAX;
+        try {
+            // Get parameters from request (if any)
+            $min = isset($_GET['min']) ? (int)$_GET['min'] : 0;
+            $max = isset($_GET['max']) ? (int)$_GET['max'] : PHP_INT_MAX;
 
-        // Validate parameters
-        if ($min < 0 || $max < 0 || $min > $max) {
-            http_response_code(400);
-            return json_encode(['error' => 'Invalid parameters']);
+            // Validate parameters
+            if ($min < 0 || $max < 0 || $min > $max) {
+                http_response_code(400);
+                echo json_encode(['error' => 'Invalid parameters']);
+                return; // stop execution
+            }
+
+            // Generate random number using Utils class
+            $random_number = Utils::getSecureRandom($min, $max);
+
+            // Return JSON response
+            echo json_encode(['random_number' => $random_number]);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-
-        // Generate random number
-        $random_number = Utils::getSecureRandom($min, $max);
-
-        // Return JSON response
-        return json_encode(['random_number' => $random_number]);
     }
 }
